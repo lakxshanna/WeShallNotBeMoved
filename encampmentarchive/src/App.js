f39a1172085efbe85943426c9dffcc88e47168ed
Lakxshanna Raveendran <lakxshanna@Lakxshannas-MacBook-Pro-3.local>
2024-11-19 23:25:33 -0500

    skeleton & netlify

---
+++ encampmentarchive/src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import InteractiveMap from './components/interactivemap';
+import Landing from './components/landing';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>We Shall Not Be Moved - IN DEV</h1>
+      <Landing />
+
+      <InteractiveMap />
+    </div>
+  );
+}
+
+export default App;
---
+++ encampmentarchive/src/components/landing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import video from '/Users/lakxshanna/Documents/WeShallNotBeMoved/encampmentarchive/src/DSC_0286.mp4'
+
+const Landing = () => {
+  return (
+    <div className='landing'>
+        <video src={video} autoPlay muted loop></video>
+        <div className='overlay'></div>
+        <div className='content'>
+            <h1>We shall not be moved</h1>
+            <p>some subheading about encampment archive</p>
+            <p>names/credit?</p>
+        </div> 
+
+    </div>
+  )
+}
+
+export default Landing
