b5e7e6752bb355fd1a3dd0f31437daefd44cf7aa
Lakxshanna Raveendran <lakxshanna@Lakxshannas-MacBook-Pro-3.local>
2024-11-26 22:54:43 -0500

    testt

--- encampmentarchive/src/components/landing.js
+++ encampmentarchive/src/components/landing.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import video from 'encampmentarchive/src/images/DSC_0286.mp4'
 
 const Landing = () => {
   return (
     <div className='landing'>
-        <video src={video} autoPlay muted loop></video>
+        <video src={`${process.env.PUBLIC_URL}/images/DSC_0286.mp4`} autoPlay muted loop></video>
         <div className='overlay'></div>
         <div className='content'>
             <h1>We shall not be moved</h1>
