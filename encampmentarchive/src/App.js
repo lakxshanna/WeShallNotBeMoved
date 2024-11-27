af907a54b8f9548119f463b6598892cbcdc343a2
Lakxshanna Raveendran <lakxshanna@Lakxshannas-MacBook-Pro-3.local>
2024-11-26 23:39:23 -0500

    Add routing to app

--- encampmentarchive/src/App.js
+++ encampmentarchive/src/App.js
@@ -3,19 +3,25 @@ import React from 'react';
 import './App.css';
 import InteractiveMap from './components/interactivemap';
 import Landing from './components/landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 function App() {
   return (
-    <div className="App">
-      <h1>We Shall Not Be Moved - IN DEV</h1>
-      <Landing />
-
-      <InteractiveMap />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>We Shall Not Be Moved - IN DEV</h1>
+        
+        {/* Define Routes */}
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/map" component={InteractiveMap} />
+          {/* You can add more routes here as needed */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
